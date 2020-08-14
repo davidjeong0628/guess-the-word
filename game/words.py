3b735d2e03d9bb36f46f59435f83edc7748f1372
David Jeong <davidjeong0628@gmail.com>
2020-08-14 11:59:35 -0700

    Implemented words.py and test_words.py

--- game/words.py
+++ game/words.py
@@ -1,22 +1,24 @@
 import random
 
 class Words:
-    """This class holds the list of words to be used in the game."""
+    """Holds the list of words to be used in the game."""
     
-    # List of words.
-    __words = load()
-    
-    @staticmethod
-    def get_rand_word() -> str:
+    def __init__(self):
+        """Initializes the list of words."""
+
+        self.list = self.load()
+
+
+    def get_rand_word(self) -> str:
         """Returns a random word from the list of words."""
-        return random.choice(__words)
+
+        return random.choice(self.list)
+
         
-    
-    @staticmethod
-    def load() -> list:
+    def load(self) -> list:
         """Loads words from 'words_alpha.txt'."""
         
-        with open('../resources/words_alpha.txt') as f:
+        with open('resources/words_alpha.txt') as f:
             words = f.readlines()
         
         return words
--- tests/test_words.py
+++ tests/test_words.py
@@ -1,5 +1,36 @@
 import unittest
 
-from .context import game 
-from game import words
+from context import game 
+from game.words import Words
 
+class WordsTestCase(unittest.TestCase):
+    """Test case for the 'game.Words' class."""
+
+    def setUp(self):
+        """Creates an instance of the 'Words' class."""
+
+        self.words = Words()
+
+    
+    def test_get_rand_word(self):
+        """Checks that a word was returned."""
+
+        for i in range(50):
+            word = self.words.get_rand_word()
+            
+            self.assertTrue(word)
+    
+    
+    def test_load(self):
+        """
+        Checks that the file containing the words was loaded correctly.
+        """
+
+        with open('resources/words_alpha.txt') as f:
+            actual_file = f.readlines()
+
+        self.assertListEqual(self.words.list, actual_file)
+
+
+if __name__ == '__main__':
+    unittest.main()

2f118931157546a4a7a9592ba3ba2e34f5860e00
David Jeong <davidjeong0628@gmail.com>
2020-08-17 16:03:03 -0700

    modified words.py and test_words.py

--- game/words.py
+++ game/words.py
@@ -1,6 +1,6 @@
 from random import choice
 
-class Words:
+class WordList:
     """Holds the list of words to be used in the game."""
     
     def __init__(self):
@@ -15,28 +15,58 @@ class Words:
         return choice(self.list)
 
 
-    def get_freq_letter(self, word: str, rank: int=1) -> str:
+    def load(self) -> list:
+        """Loads words from 'words_alpha.txt'."""
+        
+        with open('resources/words_alpha.txt') as f:
+            words = f.readlines()
+        
+        return words
+
+
+class Word:
+    """Represents an English word."""
+
+    def __init__(self, content: str):
+        """Initializes the content of the word."""
+
+        self.__content = content
+        
+        
+    def get_content(self) -> str:
+        """Returns the content of the word."""
+
+        return self.__content
+
+        
+    def get_unique_count(self) -> int:
+        """Returns the number of unique letters."""
+
+        return len(set(self.__content))
+
+
+    def get_freq_letter(self, index: int=0) -> str:
         """
-        Returns the letter with the highest frequency in 'word'.
+        Returns the letter with the highest frequency.
 
-        If 'rank' is specified, the letter with the ('rank')th highest
-        frequency is returned. 
+        If 'index' is specified, the letter at 'index' of the sorted list of
+        tuples of letters and their frequency is returned.
         """
 
-        letters_freq = self.get_letters_sort_freq(word)
+        letters_freq = self.get_letters_sort_freq()
 
-        return letters_freq[rank - 1][0]
+        return letters_freq[index][0]
 
     
-    def get_letters_sort_freq(self, word: str) -> list:
+    def get_letters_sort_freq(self) -> list:
         """
-        Returns a list of tuples of letter and its frequency in 'word' in
+        Returns a list of tuples of letter and its frequency in
         descending order based on frequency. 
         """
         
         letters_count = dict()
         
-        for letter in word:
+        for letter in self.__content:
             if letter in letters_count:
                 letters_count[letter] += 1
             else:
@@ -46,12 +76,3 @@ class Words:
             letters_count.items(), key=lambda x: x[1], reverse=True)
             
         return letters_count
-
-        
-    def load(self) -> list:
-        """Loads words from 'words_alpha.txt'."""
-        
-        with open('resources/words_alpha.txt') as f:
-            words = f.readlines()
-        
-        return words
--- tests/test_words.py
+++ tests/test_words.py
@@ -1,15 +1,15 @@
 import unittest
 
 from context import game 
-from game.words import Words
+from game.words import WordList, Word
 
-class WordsTestCase(unittest.TestCase):
-    """Test case for the 'game.Words' class."""
+class WordListTestCase(unittest.TestCase):
+    """Test case for the 'game.WordList' class."""
 
     def setUp(self):
-        """Creates an instance of the 'Words' class."""
+        """Creates an instance of 'WordList'."""
 
-        self.words = Words()
+        self.words = WordList()
 
     
     def test_get_rand_word(self):
@@ -32,5 +32,87 @@ class WordsTestCase(unittest.TestCase):
         self.assertListEqual(self.words.list, actual_file)
 
 
+class WordTestCase(unittest.TestCase):
+    """Test case for the 'game.Word' class."""
+
+    def setUp(self):
+        """Sets up words to test."""
+
+        self.std_word = 'responsible'
+        self.len_one_word = 'a'
+        self.one_uniq_word = 'aaaaaaaaaaa'
+
+    
+    def test_init(self):
+        """Checks that the 'Word' class was properly instantiated."""
+
+        self.assertEqual(
+            self.std_word, Word(self.std_word)._Word__content)
+        
+        self.assertEqual(
+            self.len_one_word, Word(self.len_one_word)._Word__content)
+
+        self.assertEqual(
+            self.one_uniq_word, Word(self.one_uniq_word)._Word__content)
+
+    
+    def test_get_content(self):
+        """Checks that word content is returned correctly."""
+
+        self.assertEqual(self.std_word, Word(self.std_word).get_content())
+
+    
+    def test_get_unique_count(self):
+        """Checks that the number of unique letters is correctly counted."""
+
+        self.assertEqual(
+            9, Word(self.std_word).get_unique_count())
+        
+        self.assertEqual(
+            1, Word(self.len_one_word).get_unique_count())
+
+        self.assertEqual(
+            1, Word(self.one_uniq_word).get_unique_count())
+
+
+    def test_get_letters_sort_freq(self):
+        """
+        Checks that the list of tuples of letters and their frequency is
+        properly sorted. 
+        """
+
+        std_word_count = [('e', 2), ('s', 2), ('r', 1), ('p', 1), ('o', 1),
+            ('n', 1), ('i', 1), ('b', 1), ('l', 1)]
+        len_one_word_count = [('a', 1)]
+        one_uniq_word_count = [('a', 11)]
+
+        self.assertListEqual(
+            std_word_count,
+            Word(self.std_word).get_letters_sort_freq())
+        
+        self.assertListEqual(
+            len_one_word_count, 
+            Word(self.len_one_word).get_letters_sort_freq())
+        
+        self.assertListEqual(
+            one_uniq_word_count, 
+            Word(self.one_uniq_word).get_letters_sort_freq())
+
+
+    def test_get_freq_letter_default(self):
+        """Checks that the letter with the highest frequency is returned."""
+
+        self.assertEqual('e', Word(self.std_word).get_freq_letter())
+        self.assertEqual('a', Word(self.len_one_word).get_freq_letter())
+        self.assertEqual('a', Word(self.one_uniq_word).get_freq_letter())
+
+
+    def test_get_freq_letter_not_default(self):
+        """Checks that the letter at the correct index is returned."""
+
+        self.assertEqual('s', Word(self.std_word).get_freq_letter(1))
+        self.assertEqual('l', Word(self.std_word).get_freq_letter(8))
+        
+        
 if __name__ == '__main__':
     unittest.main()

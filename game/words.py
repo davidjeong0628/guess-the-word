069d178f6eccc05bae10f1334f6dd7b67a479f20
David Jeong <davidjeong0628@gmail.com>
2020-08-16 19:25:17 -0700

    finished get_letters_frequency() in words.py

--- game/words.py
+++ game/words.py
@@ -14,6 +14,29 @@ class Words:
 
         return choice(self.list)
 
+    
+    def get_letters_frequency(self, word: str) -> list:
+        """
+        Returns a list of tuples of letter and its frequency in 'word'. 
+        """
+        
+        # To store letters and their frequency as keys and values,
+        # respectively.
+        letters_count = dict()
+        
+        for letter in word:
+            if letter in letters_count:
+                letters_count[letter] += 1
+            else:
+                letters_count[letter] = 1
+        
+        # Sorts 'letters_count' in descending order based on the frequency,
+        # not letters.
+        letters_count = sorted(
+            letters_count.items(), key=lambda x: x[1], reverse=True)
+
+        return letters_count
+
         
     def load(self) -> list:
         """Loads words from 'words_alpha.txt'."""
